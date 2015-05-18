0107b6ae7b19303c5b2458dae86bb5ff6dd3976d
Sébastien <sebastien.wertz@uclouvain.be>
2015-05-18 11:13:46 +0200

    Correct weight loader

--- weights_join.C
+++ weights_join.C
@@ -1,14 +1,23 @@
+#include <iostream>
+#include <fstream>
 #include <cmath>
 #include <vector>
+#include <algorithm>
+#include <cstdlib>
 
-#include "/home/fynu/swertz/memoire/scripts/utils.C"
+#include "TROOT.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TLorentzVector.h"
+#include "TMath.h"
 
-void load_weights(const char *inputFile, vector<double> &weights);
+using namespace std;
+
+void load_weights(const char *inputFile, vector< vector<double> > &weights);
 
 int weights_join(const char *inputFile_weights, const char *inputFile_tree, const char *outputFile_tree){
 
-	//gSystem->Load("~/storage/Delphes/Delphes-3.0.12/libDelphes.so");
-	gSystem->Load("libDelphes");
+	//gSystem->Load("libDelphes");
 
 	cout << "Loading data..." << endl;
 	
@@ -19,9 +28,6 @@ int weights_join(const char *inputFile_weights, const char *inputFile_tree, cons
 
 	cout << "Setting variables..." << endl;
 
-	///// input variables /////////////////////////////////////////
-
-	
 	///// output variables /////////////////////////////////////////
 	
 	// on crée un fichier root comme output:
@@ -38,19 +44,15 @@ int weights_join(const char *inputFile_weights, const char *inputFile_tree, cons
 	Tree_output->Branch("Weight_TT_Error", &O_Weight_Error);
 	Tree_output->Branch("Weighted_TT", &O_Weighted);
 	
-	///// output variables /////////////////////////////////////////
-	cout << "Loading weights... ";
+	///// loading weights /////////////////////////////////////////
+	
+  cout << "Loading weights... "  << endl;
 	cout.flush();
 
-	vector<double> weights;
-	vector<double> weightErrors;
+	vector< vector<double> > weights;
 
-	load_weights(inputFile_weights, weights, weightErrors);
+	load_weights(inputFile_weights, weights);
 
-	/*if( allentries < weights.size() ){
-		cerr << endl << "The number of events in the three categories don't add up to total number of events. Exiting..." << endl;
-		exit(1);
-	}*/
 	cout << weights.size() << " weights loaded." << endl;
 
 	cout << "Processing " << allentries << " events..." << endl;
@@ -63,9 +65,16 @@ int weights_join(const char *inputFile_weights, const char *inputFile_tree, cons
 		Tree_input->GetEntry(i);
 		
 		if(i < nbr_weights){
-			O_Weight = weights[i];;
-			O_Weight_Error = weightErrors[i];
-			O_Weighted = true;
+      if( weights[i][0] != i ){
+        cout << "Numbers don't match!" << endl;
+        exit(1);
+      }
+			O_Weight = weights[i][1];
+			O_Weight_Error = weights[i][2];
+      if(O_Weight > 0)
+			  O_Weighted = true;
+      else
+        O_Weighted = false;
 		}else{
 			O_Weight = 0.;
 			O_Weighted = false;
@@ -81,15 +90,18 @@ int weights_join(const char *inputFile_weights, const char *inputFile_tree, cons
 	
 	Tree_output->Write();
 	outputfile.Close();
+
+  return 0;
 }
 
 // this function reads the output file containing the weights,
 // sorts them (MW writes them in the wrong order) and stores them
 // in a dynamic array
 
-void load_weights(const char *inputFile, vector<double> &weights, vector<double> &weightErrors){
+bool weightSorter(vector<double> i, vector<double> j) { return i[0] < j[0]; }
 
-	vector<int> number;
+void load_weights(const char *inputFile, vector< vector<double> > &weights){
+
 	string dummy;
 	double tmp_weight, tmp_weight_error;
 	int tmp_number;
@@ -106,61 +118,59 @@ void load_weights(const char *inputFile, vector<double> &weights, vector<double>
 	
 	// loading weights
 	while( !file_weights.eof() ){
+    if( weights.size() % 1000 == 0 )
+      cout << "Reading line " << weights.size() << endl;
 		file_weights >> tmp_number >> dummy >> dummy >> tmp_weight >> tmp_weight_error; // get only number and weight & error
-		weights.push_back(tmp_weight);
-		weightErrors.push_back(tmp_weight_error);
-		number.push_back(tmp_number);
+    vector<double> tmp_vec;
+		tmp_vec.push_back(tmp_number-1);
+		tmp_vec.push_back(tmp_weight);
+		tmp_vec.push_back(tmp_weight_error);
+    weights.push_back(tmp_vec);
 	}
-	number.pop_back(); // there's one too many
-	weights.pop_back();
-	weightErrors.pop_back();
-	if(weights.size() != number.size()){
-		cerr << "Something's gone wrong in pushing back the vectors!" << endl;
-		exit(1);
-	}
+	weights.pop_back(); // there's one too many
+  cout << "Reading line " << weights.size()-1 << endl;
 
 	// ordering them:
-	for(int k=1; k < weights.size(); k++){
-		tmp_number = number[k];
-		tmp_weight = weights[k];
-		tmp_weight_error = weightErrors[k];
-		int pos = k;
-		while(tmp_number < number[pos-1]){
-			number[pos] = number[pos-1];
-			weights[pos] = weights[pos-1];
-			weightErrors[pos] = weightErrors[pos-1];
-			pos--;
-			if(pos == 0) break;
-		}
-		if(pos != k){
-			number[pos] = tmp_number;
-			weights[pos] = tmp_weight;
-			weightErrors[pos] = tmp_weight_error;
-		}
-	}
+  sort(weights.begin(), weights.end(), weightSorter);
 	
 	// making sure there's no gap:
-	if(weights.size()-1 != number[weights.size()-1]){
-		cout << endl << "There are some missing weights!" << endl;
-		for(int k=0; k<weights.size()-1; k++){
-			if(number[k]+1 != number[k+1]){
-				cout << "Weight nr. " << k+1 << " is missing." << endl;
-				number.push_back(0);
-				weights.push_back(0.);
-				weightErrors.push_back(0.);
-				for(int j=weights.size()-1; j>k+1; j--){
+	if( weights.size()-1 != weights.back().at(0) ){
+		cout << "There are some missing weights!" << endl;
+		
+    for(unsigned int k=0; k<weights.size()-2; k++){
+		  
+      vector<double> tmp_vec;
+			
+      if(weights[k][0]+1 != weights[k+1][0]){
+				
+        cout << "Weight nr. " << k+1 << " is missing." << endl;
+        
+        tmp_vec.clear();
+        tmp_vec.push_back(0);
+        tmp_vec.push_back(0);
+        tmp_vec.push_back(0);
+				weights.push_back(tmp_vec);
+				
+        for(unsigned int j=weights.size()-1; j>k+1; j--)
 					weights[j] = weights[j-1];
-					weightErrors[j] = weightErrors[j-1];
-					number[j] = number[j-1];
-				}
-				number[k+1] = k+1;
-				weights[k+1] = 0.;
-			}
-		}
-	}
+				
+        tmp_vec.clear();
+        tmp_vec.push_back(k+1);
+        tmp_vec.push_back(-1);
+        tmp_vec.push_back(0);
+				weights[k+1] = tmp_vec;
+			
+      }
+		
+    }
+	
+  }
 
-	for(int k=0; k<weights.size(); k++)
-		cout << "weight " << number[k] << ": " << weights[k] << " +- " << weightErrors[k] << endl;
+	for(unsigned int k=0; k<weights.size(); k++){
+    if( k % 1000 == 0)
+		  cout << "entry " << k <<": weight " << weights.at(k).at(0) << ": " << weights.at(k).at(1) << " +- " << weights.at(k).at(2) << endl;
+  }
+  cout << "entry " << weights.size()-1 <<": weight " << weights.back().at(0) << ": " << weights.back().at(1) << " +- " << weights.back().at(2) << endl;
 
 	file_weights.close();
 }
